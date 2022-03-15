5af87ae0303624ccbb6074249f771ffd074a61be
BootsStribling <williamstriblingenterprises@gmail.com>
2022-03-15 13:10:21 -0500

    finish lab and bonuses

---
+++ src/Score.jsx
@@ -0,0 +1,17 @@
+
+
+const Score = (props) => {
+  return ( 
+    <>
+      <ul>
+        {props.scores.map((score, idx) => 
+            <div key={idx}>
+            {score.score}-<span>{score.date}</span>
+            </div>
+            )}
+      </ul>
+    </>
+  );
+}
+
+export default Score
